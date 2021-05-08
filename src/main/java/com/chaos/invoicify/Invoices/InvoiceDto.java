3b17b71c28811a9f74ecbc9fe92cca79c174e1ed
Vinay <vinay.py@gmail.com>
2021-05-08 12:56:27 -0400

    GREEN add Invoice DTO object

---
+++ src/main/java/com/chaos/invoicify/Invoices/InvoiceDto.java
@@ -0,0 +1,17 @@
+package com.chaos.invoicify.Invoices;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InvoiceDto {
+    String invoiceName;
+    String companyName;
+    String invoiceDate;
+    List<String> itemsList;
+}
--- src/test/java/com/chaos/invoicify/Invoices/InvoiceIT.java
+++ src/test/java/com/chaos/invoicify/Invoices/InvoiceIT.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -25,7 +26,12 @@ public class InvoiceIT {
 
     @Test
     public void addInvoice() throws Exception {
-        mockMvc.perform(post("/invoices"))
+
+        InvoiceDto invoiceDto = new InvoiceDto("Invoice1", "Company1", "2021-05-08", null);
+
+        mockMvc.perform(post("/invoices")
+        .content(objectMapper.writeValueAsString(invoiceDto))
+        .contentType(MediaType.APPLICATION_JSON))
             .andExpect(status().isCreated())
             .andDo(document("AddInvoices"));
     }

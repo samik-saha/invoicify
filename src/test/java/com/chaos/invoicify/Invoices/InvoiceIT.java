930e2b10fa53d7ea6a6defc7a4fe0c319e2ad56f
Vinay <vinay.py@gmail.com>
2021-05-08 12:47:36 -0400

    GREEN - Add Empty Invoice

---
+++ src/test/java/com/chaos/invoicify/Invoices/InvoiceIT.java
@@ -0,0 +1,32 @@
+package com.chaos.invoicify.Invoices;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureRestDocs
+public class InvoiceIT {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    public void addInvoice() throws Exception {
+        mockMvc.perform(post("/invoices"))
+            .andExpect(status().isCreated())
+            .andDo(document("AddInvoices"));
+    }
+}

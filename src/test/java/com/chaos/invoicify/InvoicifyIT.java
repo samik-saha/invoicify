01aa0b20ca6d002d8f7a702ede256d4f92e967f1
Samik <3612424+samik-saha@users.noreply.github.com>
2021-05-14 17:57:57 -0400

    Added root url redirection to rest docs

---
+++ src/test/java/com/chaos/invoicify/InvoicifyIT.java
@@ -0,0 +1,26 @@
+package com.chaos.invoicify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class InvoicifyIT
+{
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void redirectRootToDocs() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().stringValues("Location", "/docs/index.html"));
+    }
+}
